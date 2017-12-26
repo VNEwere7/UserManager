e4543aea9504a65c424d95cd8da58c6201cf58ef
VNEwere7 <vewere7@gmail.com>
2017-12-26 09:38:08 +0100

    bug fixes (1)

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -4,13 +4,13 @@
 # docker build -f ./db/Dockerfile -t mongodb .
 
 # Build Express app container
-docker build -f ./app/Dockerfile -t myapp .
+# docker build -f ./app/Dockerfile -t myapp .
 
 # Configure the behaviour of the containers from docker-compose.yml
-# docker-compose up
+docker-compose up
 
 # Run MongoDB container
-docker run -i -p 27017:27017 -t mongodb
+# docker run -i -p 27017:27017 -t mongodb
 
 # Run Express app container
 docker run -i -p 3000:3000 -t myapp
