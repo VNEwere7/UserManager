c697425f566727833692f4ec8cf7de415b9816ee
VNEwere7 <vewere7@gmail.com>
2017-12-26 22:36:41 +0100

    bug fixes (3)

--- setup.sh
+++ setup.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# Build MongoDB container
-# docker build -f ./db/Dockerfile -t mongodb .
 
-# Configure the behaviour of the containers from docker-compose.yml
-docker-compose up
-
-# Run MongoDB container
-# docker run -i -p 27017:27017 -t mongodb
+touch .env
+echo "PORT=3000" >> .env
+echo "DB_URL=mongodb://localhost:27017/mongodb" >> .env
 
 # Build Express app container
 docker build -f ./Docker/app/Dockerfile -t myapp .
 
+# Configure the behaviour of the containers from docker-compose.yml
+docker-compose up -d
+
+# Build MongoDB container
+# docker build -f ./Docker/db/Dockerfile -t mongodb .
+
+# Run MongoDB container
+# docker run -i -p 27017:27017 -t mongodb
+
 # Run Express app container
-docker run -i -p 3000:3000 -t myapp
+docker run -i -p 3000:3000 --env-file=./.env -t myapp
 
 
 
